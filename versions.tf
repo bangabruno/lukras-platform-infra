b2176100aab93faad676ab774cf57b46c4cead9c
bangabruno <brunohenrisilvaa@gmail.com>
2025-10-25 23:25:42 -0300

    Implementing some changes to be more safe

--- versions.tf
+++ versions.tf
@@ -6,12 +6,4 @@ terraform {
       version = "~> 5.64"
     }
   }
-
-  backend "s3" {
-    bucket         = "lukras-platform-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "lukras-terraform-locks"
-    encrypt        = true
-  }
 }
